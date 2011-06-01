edf85bef5f12cf866330ab9b27a2355d1ed19b9b
Mugisha Moses <moses@appfrica.org>
2011-06-01 15:49:01 +0300

    conssignee loading

--- supplytracking/views.py
+++ supplytracking/views.py
@@ -7,6 +7,8 @@ from xlrd import open_workbook
 from uganda_common.utils import assign_backend
 import dateutil
 from supplytracking.utils import script_creation_handler
+from supplytracking.models import *
+
 
 class UploadForm(forms.Form):
     nodelivery = forms.BooleanField(widget=forms.CheckboxInput(),
@@ -17,27 +19,33 @@ def parse_header_row(worksheet):
     fields=['transporter','waybill','consignee','date_shipped','status']
     field_cols={}
     for col in range(worksheet.ncols):
-        value = sheet.cell(0, col).value
+        value = worksheet.cell(0, col).value
+        print value
+
         for field in fields:
-            if value.find(field) >= 0:
+            print value.lower().find(field)
+            if value.lower().find(field) >= 0:
                 field_cols[field]=col
     return field_cols
 
 
-def parse_waybill(row):
+def parse_waybill(row,worksheet,cols):
     return worksheet.cell(row, cols['waybill']).value
 
-def parse_transporter(row):
+def parse_transporter(row,worksheet,cols):
     return Contact.objects.get(name__icontains=worksheet.cell(row, cols['transporter']).value)
 
-def parse_status(row):
+def parse_status(row,worksheet,cols):
     return worksheet.cell(row, cols['status']).value
 
-def parse_consignee(row):
+def parse_consignee(row,worksheet,cols):
     return Contact.objects.get(name__icontains=(worksheet.cell(row, cols['consignee']).value))
 
-def parse_date_shipped(row):
-    return dateutil.parser.parse(worksheet.cell(row, cols['date_shipped']).value)
+def parse_date_shipped(row,worksheet,cols):
+    try:
+        date=dateutil.parser.parse(worksheet.cell(row, cols['date_shipped']).value)
+    except:
+        date=None
 
 def handle_excel_file(file):
     if  file:
@@ -46,12 +54,13 @@ def handle_excel_file(file):
         worksheet = workbook.sheet_by_index(0)
 
 
-        cols=parse_header(worksheet)
-        for row in range(sheet.nrows):
-            delivery=Delivery.objects.create(waybill=parse_waybill(row),
-                                               date_shipped=parse_date_shipped(row) ,
-                                               consignee=parse_consignee(row),
-                                               transporter=parse_transporter(row))
+        cols=parse_header_row(worksheet)
+        print cols
+        for row in range(worksheet.nrows)[1:]:
+            delivery=Delivery.objects.create(waybill=parse_waybill(row,worksheet,cols),
+                                               date_shipped=parse_date_shipped(row,worksheet,cols) ,
+                                               consignee=parse_consignee(row,worksheet,cols),
+                                               transporter=parse_transporter(row,worksheet,cols))
 
 
         if delivery:
@@ -63,7 +72,7 @@ def index(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            parse_excel_file(request.FILES['excel_file'])
+            handle_excel_file(request.FILES['excel_file'])
         else:
             form = UploadForm()
 
