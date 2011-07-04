349e47f9aca928156471b09265e4ee2bbfbf43d5
Assey Alfred <asseym@gmail.com>
2011-07-04 13:12:59 +0300

    added ability to process delivery reports with multiple waybill numbers sent by consignee into the system

--- supplytracking/app.py
+++ supplytracking/app.py
@@ -7,37 +7,43 @@ import re
 from supplytracking.models import *
 from script.models import ScriptProgress
 from script.utils.incoming import incoming_progress
+from django.core.mail import send_mail
 
 class App (AppBase):
 
     def handle (self, message):
-        waybill_reg=re.compile(r'(?P<waybill>([a-zA-Z]{2,2})/([a-zA-Z]{2,2})([0-9]{2,2})/([0-9]{5,5}))')
-        waybill_match=waybill_reg.search(message.db_message.text)
-        if waybill_match:
-            waybill=waybill_match.group('waybill')
-            delivery=Delivery.objects.get(waybill=waybill)
-            
-            #check if message is from consignee
-            if delivery.consignee.default_connection.identity==message.connection.identity:
-                delivery.status=Delivery.DELIVERED
-                delivery.save()
+        reg = re.compile('COMPLETE|DAMAGED')
+        waybills = filter(None, reg.split(message.db_message.text.upper()))
+        if len(waybills) > 0:
+            for waybill in waybills:   
+#            waybill=waybill_match.group('waybill')
+                delivery=Delivery.objects.get(waybill=waybill.strip())
                 
-                #if it is in the backlog, delete it
-                if DeliveryBackLog.objects.filter(delivery__waybill=waybill).exists():
-                    DeliveryBackLog.objects.get(delivery=delivery).delete()
-                
-                #get the response to the message if any and send it out
-                response = incoming_progress(message)
-                if response:
-                    message.respond(response)
-                return True
+                #check if message is from consignee
+                if delivery.consignee.default_connection.identity == message.connection.identity:
+                    delivery.status = Delivery.DELIVERED
+                    delivery.date_delivered = datetime.datetime.now()
+                    delivery.save()
+                    
+                    #if it is in the backlog, delete it
+                    if DeliveryBackLog.objects.filter(delivery__waybill=waybill).exists():
+                        DeliveryBackLog.objects.get(delivery=delivery).delete()
+                    
+            #in case consignee has sent delivery report, send it out to supply admins        
+            if delivery.consignee.default_connection.identity == message.connection.identity:
+                supply_admins = Contact.objects.filter(groups__name__in=['supply_admin'])
+                recipients = list(supply_admins.values_list('user__email',flat=True).distinct())
+                sender = 'no-reply@uganda.rapidsms.org'
+                subject = 'SupplyTracking: Delivery Report'
+                message = message.db_message.text
+                if message.strip():
+                    send_mail(subject, message, sender, recipients, fail_silently=False)  
             
-            #if message is from the transporter simply process the response and send it to transporter
-            elif delivery.transporter.default_connection.identity == message.connection.identity:
-                response = incoming_progress(message)
-                if response:
-                    message.respond(response)
-                return True                     
+            #process and send out response to message sender
+            response = incoming_progress(message)
+            if response:
+                message.respond(response)
+            return True      
         else:
             return False
 
